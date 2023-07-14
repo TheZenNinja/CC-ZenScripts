1a5bda96024d707a28bc79232d9425ac4c6bdde6
TheZenNinja <65081429+TheZenNinja@users.noreply.github.com>
2023-07-14 14:01:41 -0400

    Worked on button

--- PackageManager/Packages/button.lua
+++ PackageManager/Packages/button.lua
@@ -1,5 +1,38 @@
 local monitor = peripheral.find("monitor")
 
+--#region Functions
+function CreateButton(xPos,yPos,width,height,label)
+    local oldBG = monitor.getBackgroundColor()
+    local oldFG = monitor.getTextColor()
+
+    monitor.setBackgroundColor(colors.white)
+    monitor.setTextColor(colors.black)
+
+    for y = yPos,yPos+height do
+        for x = xPos,xPos+width do
+            monitor.setCursorPos(x,y)
+        end
+    end
+
+    local labelCenterX = math.floor(xPos + (width/2) - (#label/2))
+    local labelCenterY = math.floor(yPos + #height/2)
+
+    monitor.setCursorPos(labelCenterX, labelCenterY)
+    monitor.write(label)
+
+    monitor.setBackgroundColor(oldBG)
+    monitor.setTextColor(oldFG)
+    
+end
+function CreateButtonFromBottomRight(x,y,width,height,label)
+    local sizeX, sizeY = monitor.getSize()
+    local x2 = x
+    local y2 = sizeY-y-height
+    CreateButton(x2,y2,width,height,label)
+end
+--#endregion
+
+
 while true do
     local event, side, x, y = os.pullEvent("monitor_touch")
 
